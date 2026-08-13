09a067bc126dc04a506118620740e733810477d0
Gagan Yadav <ygagan012@gmail.com>
2026-08-13 21:17:28 +0530

    add painter's partition problem file

--- leetcode/painters.java
+++ leetcode/painters.java
@@ -1,6 +1,30 @@
 package leetcode;
 
 public class painters {
+
+    static boolean isValidSolution(int boards[] , int k ,int mid){
+        int paintercount = 1;
+        int painterlength = 0;
+        int maxlength = mid;
+
+        for(int i=0; i<boards.length; i++){
+            if(painterlength+boards[i]<=maxlength){
+                painterlength = painterlength + boards[i];
+            }
+            else{
+                paintercount++;
+                painterlength=0;
+
+                if(paintercount > k || boards[i] > maxlength){
+                    return false;
+                }
+                else{
+                    painterlength = painterlength + boards[i];
+                }
+            }
+        }
+        return true;
+    }
     
     public int mintime(int[] boards,int k){
         int sum= 0;
