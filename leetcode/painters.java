48457b0e5f7d5c886190b245fb26decbb9cf6c8a
Gagan Yadav <ygagan012@gmail.com>
2026-08-13 21:10:39 +0530

    add: implement mintime method to calculate minimum time for painters

--- leetcode/painters.java
+++ leetcode/painters.java
@@ -2,4 +2,27 @@ package leetcode;
 
 public class painters {
     
+    public int mintime(int[] boards,int k){
+        int sum= 0;
+        for(int i=0; i<boards.length; i++){
+            sum=sum+boards[i];
+        }
+        int ans = -1;
+        int s =0;
+        int e = sum;
+
+        while(s<=e){
+            int mid = s + (e-s)/2;
+
+            if(isValidSolution(mid,k,boards)){
+                ans = mid;
+                e = mid-1;
+            }
+            else{
+                //not valid
+                s = mid+1;
+            }
+        }
+        return ans;
+    }
 }
