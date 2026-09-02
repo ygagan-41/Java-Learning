234288fcc9026ab909869c770d62f4a17cd5ffd9
Gagan Yadav <ygagan012@gmail.com>
2026-09-02 15:04:32 +0530

    changes

---
+++ leetcode/subsetII.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class subsetII {
+     static void solve(int[]nums , int index , List<Integer> output,List<List<Integer>> ans){
+        //base case
+        //subsequnece ready hai
+        if(index >= nums.length){
+            ans.add(new ArrayList<>(output));
+            return;
+        }
+        //1 case hum baaki recursion
+        //include and exclude pattern 
+        int CurrValue = nums[index];
+
+        //include
+        output.add(CurrValue);
+        solve(nums, index+1, output, ans);
+
+        //backtracking
+        //output vesa hi kardo jese vo pehle tha yaani empty or null
+        output.remove(output.size()-1);
+
+        //while excluding , ignore the same elements and move to the new element 
+        while(index+1 < nums.length && nums[index] == nums[index+1]){
+            index++;
+        }
+        //exclude
+        solve(nums, index+1, output, ans);
+    }
+    public List<List<Integer>> Subsets(int[] nums){
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        int index = 0;
+        solve(nums,index,output,ans);
+        return ans;
+    }
+}
